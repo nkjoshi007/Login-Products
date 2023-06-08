ed3888bba4067e85a8e9ad52d82ff4b14ef2a714
naveensharma <sharmanaveen0010@gmail.com>
2023-06-08 12:49:15 +0530

    complete Add to cart

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
-import { AppBar, Box, Button, Container, FormControlLabel, Toolbar } from "@mui/material"
+import { AppBar, Badge, Box, Button, Container, FormControlLabel, Toolbar } from "@mui/material"
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link, Outlet } from "react-router-dom"
+import { useSelector } from "react-redux";
 
 export const Home =()=>{
+    const Cart_len =useSelector((state)=>state.cartreducer.Cart)
+    console.log("length",Cart_len);
     return(
         <>
         <AppBar position="sticky">
@@ -15,8 +18,8 @@ export const Home =()=>{
                    <h3 style={{marginTop:"5vh",marginLeft:'3rem'}}><Link to='/Products' className="link">Products</Link></h3>
                    <h3 style={{marginTop:"5vh",marginLeft:'3rem'}}><Link to='/Contactus' className="link">Contact us</Link></h3>
                    <FormControlLabel sx={{marginLeft:"auto"}}  control={<Button color='secondary' variant='contained'><Link to='/' className="link">Signup</Link></Button>}/>
-                   <FormControlLabel sx={{marginLeft:"auto"}}  control={<Link to='/cart' className="link"><ShoppingCartIcon/></Link>}/>
-                 </Box>
+                   <FormControlLabel sx={{marginLeft:"auto"}}  control={<Link to='/cart' className="link"><Badge badgeContent={Cart_len.length} color="secondary"><ShoppingCartIcon fontSize="large"  sx={{color:"black"}}/></Badge></Link>}/>
+                  </Box>
                 </Toolbar>
             </Container>
         </AppBar>
